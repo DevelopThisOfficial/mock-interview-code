5d92fdd8ddd5bf35b9020492a9b337497a9cdcf7
Jordan Masone <jordanmasone1@gmail.com>
2022-06-12 17:29:05 -0400

    Added starter code.

---
+++ react-js-starter/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+
+    return (
+        <>
+            <div className="mx-auto w-75 p-3">
+                <div className="d-flex">
+                    <h2>Random Pokemon Generator</h2>
+                    <button className="btn btn-primary btn-sm w-20 ms-auto">
+                        Random Pokemon
+                    </button>
+                </div>
+            </div>
+            <hr />
+        </>
+    )
+}
